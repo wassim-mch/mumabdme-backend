6dbf450504adad2660282b72776f19cbd7432997
houssam-elddine <vortixdev@gmail.com>
2025-12-10 01:47:50 +0100

    feat : Add permission middleware to services API resource

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\CategorieController;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('services', ServiceController::class);
+    Route::apiResource('services', ServiceController::class)->middleware('permission:manage_services');
     Route::apiResource('categories', CategorieController::class)->middleware('permission:manage_categories');
     Route::post('/logout', [AuthController::class, 'logout']);
 });
